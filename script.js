65b04170a01f78313713af18ebf403cfb43cdd7d
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-02-19 17:53:51 +0400

    define the snake and gave positions on canvas, create two functions to give cordinates and colors to snake and second function to show the snake on the canves with given cordinates and colors

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const GameBoard = document.getElementById("GameBoard");
 const context = GameBoard.getContext("2d");
+
+let snake = [
+    {x:250, y:250},
+    {x:240, y:250},
+    {x:230, y:250},
+    {x:220, y:250},
+    {x:210, y:250}
+];
+
+function drawSquare(xCordinate, yCordinate, squareColor, borderColor) {
+    context.fillStyle = squareColor;
+    context.strokeStyle = borderColor;
+    context.fillRect(xCordinate, yCordinate, 10, 10);
+    context.strokeRect(xCordinate, yCordinate, 10, 10);
+}
+
+function showSnake() {
+    snake.forEach(element => drawSquare(element.x, element.y, 'lightblue', 'black'));
+}
+
+showSnake();
